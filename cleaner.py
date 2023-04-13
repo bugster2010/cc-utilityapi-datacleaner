ae577831ec63fdb41bb3e0820ec3dbdc2b05481d
Tyler L <69596889+tylew@users.noreply.github.com>
2023-04-13 14:47:01 -0700

    Update repo

--- cleaner.py
+++ cleaner.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 from datetime import datetime
 from datetime import timedelta
@@ -9,22 +8,7 @@ entries_per_week = 672
 class DataCleaningError(Exception):
     pass
 
-def to_datetime(date_string):
-    # Try parsing the string using the '%m/%d/%Y %H:%M' format first
-    try:
-        date_obj = datetime.strptime(date_string, '%m/%d/%Y %H:%M')
-    except ValueError:
-        # If that fails, try using the '%Y-%m-%d %H:%M:%S' format
-        try:
-            date_obj = datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S')
-        except ValueError:
-            # If that fails, raise a DataCleaningError exception
-            raise DataCleaningError(f"Could not convert {date_string} to datetime")
-    return date_obj
-
-
 def create_clean_dataframe(df):
-
     # Check for missing column names, throw error if any are missing
     required_columns = ['interval_start', 'interval_end']
     missing_columns = set(required_columns) - set(df.columns)
@@ -32,19 +16,20 @@ def create_clean_dataframe(df):
     if missing_columns:
         raise DataCleaningError('Missing columns: {}'.format(', '.join(missing_columns)))
 
+    # Convert the 'interval_start' and 'interval_end' columns to datetime objects
+    df['interval_start'] = pd.to_datetime(df['interval_start'], format='%m/%d/%Y %H:%M', errors='coerce')
+    df['interval_end'] = pd.to_datetime(df['interval_end'], format='%m/%d/%Y %H:%M', errors='coerce')
 
     cleaned_data = []
     
-    for ind in df.index:
-
-        # Store variables for current index of dataframe
-        interval_start = to_datetime(df['interval_start'][ind])
-        interval_end = to_datetime(df['interval_end'][ind])
+    for ind, row in df.iterrows():
+        interval_start = row['interval_start']
+        interval_end = row['interval_end']
         _timedelta = interval_end - interval_start
         
         if (_timedelta == timedelta(minutes = 15)):
             # proper entry, no modification need
-            cleaned_data.append(df.iloc[ind])
+            cleaned_data.append(row)
         else:
             # There is data missing in this set at the current index
             # Will copy usage data from week prior for most-accurate fix
@@ -54,8 +39,8 @@ def create_clean_dataframe(df):
             
             while (ph_interval_end - interval_start >= timedelta(minutes = 15)):
                 copy_curr = df.iloc[copy_index].copy()
-                copy_curr.interval_end = ph_interval_end.strftime('%m/%d/%Y %H:%M')
-                copy_curr.interval_start = (ph_interval_end - timedelta(minutes = 15)).strftime('%m/%d/%Y %H:%M')
+                copy_curr.interval_end = ph_interval_end.strftime('%Y-%m-%d %H:%M:%S')
+                copy_curr.interval_start = (ph_interval_end - timedelta(minutes = 15)).strftime('%Y-%m-%d %H:%M:%S')
                 
                 cleaned_data.append(copy_curr)
                 
--- gui.py
+++ gui.py
@@ -4,8 +4,9 @@ from tkinter import messagebox
 from PIL import ImageTk, Image
 import pandas as pd
 import os
+import sys
 
-import cleaner # Import the cleaner.py file
+import cleaner  # Import the cleaner.py file
 
 # Set up the GUI
 root = tk.Tk()
@@ -17,8 +18,15 @@ root.resizable(False, False)
 file_label = None
 
 
+def get_path(filename):
+    if hasattr(sys, "_MEIPASS"):
+        return os.path.join(sys._MEIPASS, filename)
+    else:
+        return filename
+
+
 # Add Clean Coalition logo and name
-logo_image = Image.open("clean_coalition_logo.png")
+logo_image = Image.open(get_path("assets/logo.png"))
 width, height = logo_image.size
 aspect_ratio = height / width
 new_width = 350
@@ -32,6 +40,7 @@ logo_label.grid(row=0, column=0, padx=10, pady=10, columnspan=2)
 def show_info():
     messagebox.showinfo("Clean Coalition CSV Tool", "This program cleans and exports UtilityAPI CSV files with date/time discrepancies typically occurring during daylight savings.\n\nPlease select a CSV file to upload \n\nThe program will clean it up and export the cleaned data.\n\n© 2023 Clean Coalition. All rights reserved.")
 
+
 # Function to upload CSV file
 def upload_file():
     global file_label
@@ -43,24 +52,28 @@ def upload_file():
         file_label.destroy()
 
     file_label = tk.Label(root, text='file: ' + os.path.basename(file_path), font=("Arial", 16), bg="#0072C6", fg="#FFFFFF")
-    file_label.grid(row=5, column=0)
-    file_label.update() 
+    file_label.grid(row=3, column=0, sticky='W', padx=10)
+    file_label.update()
     if file_path:
         df = pd.read_csv(file_path)
         process_data(df)
 
+
 # Function to save cleaned CSV data
-def save_file(df):
+def save_file(df, reverse_option):
     file_path = filedialog.asksaveasfilename(defaultextension=".csv")
     if file_path:
+        if reverse_option.get() == "Reverse":
+            df = df.iloc[::-1]
         df.to_csv(file_path, index=False)
         messagebox.showinfo(title="Export Complete", message="Data cleaned and saved successfully!")
-        
+
+
 # Function to process CSV data
 def process_data(df):
     # Replace NaN values with 0
     df.fillna(0, inplace=True)
-    
+
     # Create a new Toplevel window for the loading state
     loading_window = tk.Toplevel(root)
     loading_window.title("Processing Data")
@@ -68,19 +81,24 @@ def process_data(df):
     loading_label = tk.Label(loading_window, text="Processing data, please wait...", font=("Arial", 12))
     loading_label.pack(pady=20)
     loading_window.update()  # Force update to display loading window
-    
+
     # Use the cleaner module to clean the data
     df = cleaner.create_clean_dataframe(df)
-    
+
     # Destroy the loading window and display the results
     loading_window.destroy()
-    save_button = tk.Button(root, text="Export Cleaned Data", command=lambda: save_file(df), font=("Arial", 16), bg="#FFFFFF")
-    save_button.grid(row=3, column=0, pady=10, padx=10, columnspan=2)
-    # result_label = tk.Label(root, text="Data processed!", font=("Arial", 16), bg="#0072C6", fg="#FFFFFF")
-    # result_label.grid(row=2, column=1)
+    # Add a variable to store the selected option from the dropdown
+    reverse_option = tk.StringVar(root)
+    reverse_option.set("No Reverse")
 
+    # Add a dropdown menu to select reverse option
+    reverse_dropdown = tk.OptionMenu(root, reverse_option, "No Reverse", "Reverse")
+    reverse_dropdown.config(font=("Arial", 12), bg="#FFFFFF")
+    reverse_dropdown.grid(row=4, column=1)
 
-
+    # Modify the save_button command to include the reverse_option variable
+    save_button = tk.Button(root, text="Export Cleaned Data", command=lambda: save_file(df, reverse_option), font=("Arial", 16), bg="#FFFFFF")
+    save_button.grid(row=4, column=0, pady=10, padx=10, columnspan=1)
 
 # Add Upload CSV button
 upload_button = tk.Button(root, text="Upload CSV", command=upload_file, font=("Arial", 16), bg="#FFFFFF")
@@ -106,3 +124,4 @@ root.grid_columnconfigure(1, weight=1)
 
 # Start the GUI
 root.mainloop()
+
