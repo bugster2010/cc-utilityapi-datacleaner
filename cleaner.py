9f4f74cd4923b2c27f6893047085b7207d4c52b8
Tyler L <69596889+tylew@users.noreply.github.com>
2023-05-02 11:00:06 -0700

    update cleaning algo
    wouldn't account for inconsistency resulting from previous row

--- cleaner.py
+++ cleaner.py
@@ -21,10 +21,35 @@ def create_clean_dataframe(df):
     df['interval_end'] = pd.to_datetime(df['interval_end'], format='%m/%d/%Y %H:%M', errors='coerce')
 
     cleaned_data = []
+    prev_start = None
     
     for ind, row in df.iterrows():
         interval_start = row['interval_start']
         interval_end = row['interval_end']
+
+
+        # Check if there is inconsistency from previous row
+        if (interval_end != prev_start and prev_start is not None):
+            print("prev row inconsistent")
+            # inconsistency between previous entry and current 
+            copy_index = ind - entries_per_week
+
+            while (prev_start != interval_end):
+                
+                # insert into df: 
+                copy_curr = df.iloc[copy_index].copy()
+                copy_curr.interval_end = prev_start.strftime('%Y-%m-%d %H:%M:%S')
+                copy_curr.interval_start = (prev_start - timedelta(minutes = 15)).strftime('%Y-%m-%d %H:%M:%S')
+                print(interval_end)
+
+                cleaned_data.append(copy_curr)
+
+                copy_index += 1
+
+                prev_start = prev_start - timedelta(minutes = 15)
+
+
+        prev_start = interval_start
         _timedelta = interval_end - interval_start
         
         if (_timedelta == timedelta(minutes = 15)):
@@ -45,6 +70,7 @@ def create_clean_dataframe(df):
                 cleaned_data.append(copy_curr)
                 
                 ph_interval_end = ph_interval_end - timedelta(minutes = 15)
+                copy_index += 1
 
     cleaned_df = pd.DataFrame(cleaned_data)
 
