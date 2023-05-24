56f6a35bdd6aa2be50c6b4408610e3b54fb9d4e0
Matthew Graham <bugster2010@yahoo.com>
2023-05-24 16:56:58 -0700

    fixed intervals using saved data

--- clean.py
+++ clean.py
@@ -20,7 +20,7 @@ def removeDiscrepencies(df):
         raise DataCleaningError('Missing columns: {}'.format(', '.join(missing_columns)))
 
     
-    
+    df = reverseDF(df)
 
     # Convert the 'interval_start' and 'interval_end' columns to datetime objects
     df['interval_start'] = pd.to_datetime(df['interval_start'], format='%m/%d/%Y %H:%M', errors='coerce')
@@ -28,50 +28,63 @@ def removeDiscrepencies(df):
 
     cleanDF = []
 
+
     #begin for loop for iteration through data
     #index is given the int of the index and row is the data
     #set the lowest time possible
     currHighestTime = datetime(year=1,month=1,day=1,hour=0,minute=0,second=0)
-    # Reverse the dataset so it is in order
-    df = reverseDF(df)
+    
+    flag = 0
     for index, row in df.iterrows():
-        
+
         prev = index - 1
+        cleanRow = row
+        
 
         #validate start time is 15 minutes from end time
         timeDifference = row['interval_end'] - row['interval_start']
         if(timeDifference != timedelta(minutes = 15)):
             #fix to the proper time difference
-            print(row['interval_start'], row['interval_end'])
-            row['interval_end'] = row['interval_start'] + timedelta(minutes = 15)
-            print(row['interval_start'], row['interval_end'])
+            print(cleanRow['interval_end'], cleanRow['interval_start'])
+            cleanRow['interval_end'] = row['interval_start'] + timedelta(minutes = 15)
+            print(cleanRow['interval_end'], cleanRow['interval_start'])
+            print(cleanDF[-1].interval_start, cleanDF[-1].interval_end, cleanRow['interval_start'], cleanRow['interval_end'])
+            flag = 2
+            
+            
         
         #validate there is no repeat
-        if(row['interval_start'] < currHighestTime):
+        if(cleanRow['interval_start'] < currHighestTime):
             #this effectively skips anything that starts before what we have already deemed has ended 
             continue
 
-        currHighestTime = row['interval_end']
+        currHighestTime = cleanRow['interval_end']
+
+        if(flag != 0):
+                print("flag tripped:", cleanDF[-1].interval_end, cleanRow['interval_start'])
+                print(cleanDF[-1].interval_end != cleanRow['interval_start'])
+                flag = flag - 1
 
         #validate all times exist
-        if(0 <= prev <= len(df.index)):
-            if(df.iloc[prev].interval_end != df.iloc[index].interval_start):
+        if(len(cleanDF)>= 1):
 
+            if(cleanDF[-1].interval_end != cleanRow['interval_start']):
                 #if it is greater then fill in a blank
-                #if(df.iloc[prev, 'interval_end'] <= row['interval_start']): <- this should never happen
-                    
                 #use a week ago to fill in the blank, otherwise use a week forward
                 if(0<= (index - entries_per_week) <= len(df.index)):
                     copyInd = index - entries_per_week
-                    print(1)
+                    print("Fetching Week Prior")
                 else:
-                    print(2)
+                    print("Fetching Week Forward")
                     copyInd = index + entries_per_week
                 
-                copyIntervalStart = df.iloc[prev].interval_end
+                copyIntervalStart = cleanDF[-1].interval_end
                 copyIntervalEnd = row['interval_start']
+                print("Creating Copied data between", copyIntervalStart, "and", copyIntervalEnd)
+
 
                 while(copyIntervalEnd != copyIntervalStart):
+                    
                     copyInd += 1
                     
                     copy_curr = df.iloc[copyInd].copy()
@@ -80,10 +93,9 @@ def removeDiscrepencies(df):
                 
                     copyIntervalStart += timedelta(minutes=15)
                     cleanDF.append(copy_curr)
-                    break
+                    
         
-        cleanDF.append(row)
-            
+        cleanDF.append(cleanRow)
 
         
 
@@ -107,5 +119,6 @@ def testScript():
     data = pd.read_csv(testfile)
     data = removeDiscrepencies(data)
     data.to_csv("assets/csv/testfile.csv")
+
 
 testScript()
